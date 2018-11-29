58c2d45d34e2334eaa10555a2171962a21b6d20e
atafoa <atafoa@yahoo.com>
2018-11-29 17:10:05 -0600

    Added more tests for post class

--- testing/testPosts.cpp
+++ testing/testPosts.cpp
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(testMakePosts)
     dummy_user.set_interests(interests);
 
     BOOST_TEST_PASSPOINT();
-    std::cout << "User can be created: PASS" << std::endl;
+    std::cout << "User can be created: PASS\n" << std::endl;
 
     //make post and send it to post class
     //write post to users saved post
@@ -37,18 +37,35 @@ BOOST_AUTO_TEST_CASE(testMakePosts)
     {
         samplePost += "Hi";
     }
-
+    
+    Post testPost;
     std::cout << "Sample post is: " << samplePost << std::endl;
-    dummy_user.set_post_singular(samplePost,1);
-
+    testPost.enter_post_data(samplePost);
     BOOST_TEST_PASSPOINT();
-    std::cout << "Post can be made: Pass" << std::endl;
+    std::cout << "Post can be made: Pass\n" << std::endl;
+
+    //assign each post a serial number
+    std::cout << "Setting the serial number to 1" << std::endl;
+    testPost.set_serial_number(1);
+    BOOST_TEST_PASSPOINT();
+    std::cout << "Each post is assigned a serial number: Pass\n" << std::endl;
 
     //write post to file
     dummy_user.write_to_file();
-
     BOOST_TEST_PASSPOINT();
-    std::cout << "Posts can be saved on disk: Pass" << std::endl;
+    std::cout << "Posts can be saved on disk: Pass\n" << std::endl;
+
+    //check posts are stored accurately
+    std::string temp = testPost.get_post_data();
+    std::cout << "The post that was made was: " << temp << std::endl;
+    BOOST_TEST_PASSPOINT();
+    std::cout << "Posts made can be retrieved: Pass\n" << std::endl;
+
+    //check serial numbers are stored accurately
+    int tempSNO = testPost.get_serial_number();
+    std::cout << "Serial number for this post is: " << tempSNO << std::endl;
+    BOOST_TEST_PASSPOINT();
+    std::cout << "Serial numbers are stored accurately: Pass\n" << std::endl;
 
 }
 
